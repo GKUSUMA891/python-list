1e033f3cc685983b2894b485ea779a89294faba8
kusuma <kusuma.g@thundersoft.com>
2022-04-26 01:25:28 -0700

    clearing a list in different ways

---
+++ clearlist.py
@@ -0,0 +1,27 @@
+#clear the elements in list
+#using clear()
+list1=[10,30,49,67]
+list2=[12,34,56,78]
+print("before clearing the list1",list1)
+list1.clear()
+print("after clearing the list1",list1)
+
+print("before clearing the list2",list2)
+list2.clear()
+print("after clearing the list2",list2)
+#using reinitialization []
+list_t=[9,108,107,1065]
+list_t1=[1,902,803,604,205]
+print("before clearing the list_t",list_t)
+list_t=[]
+print("after clearing the list_t",list_t)
+
+print("before clearing the list_t1",list_t1)
+list_t1=[]
+print("after clearing the list_t1",list_t1)
+#using *=0
+listt=[10,23,45,67,89]
+print("before clearing the listt",listt)
+listt *=0
+print("after clearing the listt",listt)
+
